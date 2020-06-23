26128607b58d97a70eefb0b0486a1c46b83baa4a
AnonymousAIStudent <67345010+AnonymousAIStudent@users.noreply.github.com>
2020-06-24 00:36:36 +0300

    Add files via upload

---
+++ mcboy.py
@@ -0,0 +1,95 @@
+# Import the API objects
+from api import State, util
+from .Node import Node
+from .MonteCarloTreeSearch import MonteCarloTreeSearch
+import random
+import time
+
+class Bot:
+	
+	__max_depth = 12
+	__randomize = True
+
+	def __init__(self):
+		pass
+
+	def get_move(self, state):
+		if state.get_phase() == 1:
+			initial_board_state = state.make_assumption()
+			root = Node(initial_board_state)
+			mcts = MonteCarloTreeSearch(root)
+			start_time = time.time()
+			best_move = mcts.best_move(5000)
+			end_time = time.time()
+			print(end_time - start_time)
+			return best_move
+		else:
+			_, move = self.value(state)
+
+			return move
+
+	def value(self, state, alpha=float('-inf'), beta=float('inf'), depth = 0):
+		"""
+		Return the value of this state and the associated move
+		:param State state:
+		:param float alpha: The highest score that the maximizing player can guarantee given current knowledge
+		:param float beta: The lowest score that the minimizing player can guarantee given current knowledge
+		:param int depth: How deep we are in the tree
+		:return val, move: the value of the state, and the best move.
+		"""
+		if state.finished():
+			winner, points = state.winner()
+			return (points, None) if winner == 1 else (-points, None)
+
+		if depth == self.__max_depth:
+			return heuristic(state)
+
+		best_value = float('-inf') if maximizing(state) else float('inf')
+		best_move = None
+
+		moves = state.moves()
+
+		if self.__randomize:
+			random.shuffle(moves)
+
+		for move in moves:
+			next_state = state.next(move)
+			value, _ = self.value(next_state, alpha, beta, depth + 1)
+
+			if maximizing(state):
+				if value > best_value:
+					best_value = value
+					best_move = move
+					alpha = best_value
+			else:
+				if value < best_value:
+					best_value = value
+					best_move = move
+					beta = best_value
+
+			# Prune the search tree
+			# We know this state will never be chosen, so we stop evaluating its children
+			if alpha >= beta:
+				break
+
+		return best_value, best_move
+
+def maximizing(state):
+	# type: (State) -> bool
+	"""
+	Whether we're the maximizing player (1) or the minimizing player (2).
+
+	:param state:
+	:return:
+	"""
+	return state.whose_turn() == 1
+
+def heuristic(state):
+	# type: (State) -> float
+	"""
+	Estimate the value of this state: -1.0 is a certain win for player 2, 1.0 is a certain win for player 1
+
+	:param state:
+	:return: A heuristic evaluation for the given state (between -1.0 and 1.0)
+	"""
+	return util.ratio_points(state, 1) * 2.0 - 1.0, None
